75c89fa9909884798deef04f8f593b6637dcaf72
SavorTheFlavor <2500364530@qq.com>
2018-08-18 20:39:35 +0800

    some concurrent collections
    SkipList(跳表)
    1----------------->6------------->10
    1--------->4----->6----->8----->10
    1->2->3->4->5->6->7->8->9->10

--- src/main/java/com/me/concurrency/example/concurrent/ConcurrentSkipListSetExample.java
+++ src/main/java/com/me/concurrency/example/concurrent/ConcurrentSkipListSetExample.java
@@ -1,18 +1,18 @@
-package com.me.concurrency.example.syncContainer;
+package com.me.concurrency.example.concurrent;
 
-import com.google.common.collect.Lists;
 import com.me.concurrency.annotation.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Vector;
-import java.util.concurrent.*;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
-//Collections.synchronizedList(...)
 @Slf4j
 @ThreadSafe
-public class CollectionsExample1 {
+public class ConcurrentSkipListSetExample {
 
     // 请求总数
     public static int clientTotal = 5000;
@@ -20,7 +20,7 @@ public class CollectionsExample1 {
     // 同时并发执行的线程数
     public static int threadTotal = 200;
 
-    private static List<Integer> list = Collections.synchronizedList(Lists.newArrayList());
+    private static Set<Integer> set = new ConcurrentSkipListSet<>();
 
     public static void main(String[] args) throws Exception {
         ExecutorService executorService = Executors.newCachedThreadPool();
@@ -41,10 +41,10 @@ public class CollectionsExample1 {
         }
         countDownLatch.await();
         executorService.shutdown();
-        log.info("size:{}", list.size());
+        log.info("size:{}", set.size());
     }
 
     private static void update(int i) {
-        list.add(i);
+        set.add(i);
     }
 }
--- src/main/java/com/me/concurrency/example/concurrent/CopyOnWriteArrayListExample.java
+++ src/main/java/com/me/concurrency/example/concurrent/CopyOnWriteArrayListExample.java
@@ -1,18 +1,20 @@
-package com.me.concurrency.example.syncContainer;
+package com.me.concurrency.example.concurrent;
 
-import com.google.common.collect.Lists;
 import com.me.concurrency.annotation.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
-import java.util.concurrent.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
-//Collections.synchronizedList(...)
+//写的时候加锁，读无需加锁
 @Slf4j
 @ThreadSafe
-public class CollectionsExample1 {
+public class CopyOnWriteArrayListExample {
 
     // 请求总数
     public static int clientTotal = 5000;
@@ -20,7 +22,7 @@ public class CollectionsExample1 {
     // 同时并发执行的线程数
     public static int threadTotal = 200;
 
-    private static List<Integer> list = Collections.synchronizedList(Lists.newArrayList());
+    private static List<Integer> list = new CopyOnWriteArrayList<>();
 
     public static void main(String[] args) throws Exception {
         ExecutorService executorService = Executors.newCachedThreadPool();
