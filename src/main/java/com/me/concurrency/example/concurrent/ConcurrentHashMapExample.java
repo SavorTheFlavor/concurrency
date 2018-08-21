121cb5d5f60a60fd5ae47a0aedd604c14d127402
SavorTheFlavor <2500364530@qq.com>
2018-08-21 23:45:14 +0800

    FutureTask and ForkJoinTask

--- src/main/java/com/me/concurrency/example/concurrent/ConcurrentHashMapExample.java
+++ src/main/java/com/me/concurrency/example/concurrent/ConcurrentHashMapExample.java
@@ -1,6 +1,6 @@
 package com.me.concurrency.example.concurrent;
 
-import com.mmall.concurrency.annoations.ThreadSafe;
+import com.me.concurrency.annotation.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
